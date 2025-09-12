0134faba5bd0754c3da02a8c6f5f4f5301755834
sahuranjit8025 <sahuranjit8025@gmail.com>
2025-09-12 17:54:55 +0530

    app.js file changed for welcome message

--- app.js
+++ app.js
@@ -7,7 +7,7 @@
     const server = http.createServer((req, res) => {
       res.statusCode = 200;
       res.setHeader('Content-Type', 'text/plain');
-      res.end('welcome to Node.js CI/CD! in jenkins');
+      res.end('welcome to Node.js CI/CD! in jenkins from Ranjit');
     });
 
     server.listen(port, hostname, () => {
